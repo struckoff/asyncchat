681eee6755c87d9c66c481333dda08a16c120fff
struckoff <as5423.ru@gmail.com>
2015-08-14 02:00:47 +0300

    pep fix

--- server/lib.py
+++ server/lib.py
@@ -3,79 +3,82 @@ import json
 import hashlib
 import time
 
+
 class Room_class(object):
-	"""Simple class of a simple room"""
+    """Simple class of a simple room"""
 
-	def __init__(self,password = False):
-		self.user_list   = {}
-		self.password    = password
+    def __init__(self, password=False):
+        self.user_list = {}
+        self.password = password
 
-	@asyncio.coroutine
-	def onDisconnect(self,client,reason = 'Access denied',clean = False):
-		if clean and not client.open:
-			if self.user_list.get(client,False):
-				self.user_list.pop(client)
-			for client_item in self.user_list:
-				yield from client_item.send(json.dumps({'user_list':list(self.user_list.values())}))
-		else:
-			yield from client.send(json.dumps({'error':reason}))
-			yield from client.close(reason = reason)
+    @asyncio.coroutine
+    def onDisconnect(self, client, reason='Access denied', clean=False):
+        if clean and not client.open:
+            if self.user_list.get(client, False):
+                self.user_list.pop(client)
+            for client_item in self.user_list:
+                yield from client_item.send(json.dumps({'user_list': list(self.user_list.values())}))
+        else:
+            yield from client.send(json.dumps({'error': reason}))
+            yield from client.close(reason=reason)
 
-		data_json = {'user_list':list(self.user_list.values())}
-		data_json = json.dumps(data_json)
+        data_json = {'user_list': list(self.user_list.values())}
+        data_json = json.dumps(data_json)
 
-		for client_item in self.user_list:
-			yield from client_item.send(data_json)	
+        for client_item in self.user_list:
+            yield from client_item.send(data_json)
 
-	@asyncio.coroutine
-	def onConnect(self,client,data):
-		"""Second level hendshake"""
+    @asyncio.coroutine
+    def onConnect(self, client, data):
+        """Second level hendshake"""
 
-		self.user_list[client] = data.get('name')
+        self.user_list[client] = data.get('name')
 
-		data_json = {
-					'user_list':list(self.user_list.values()),
-					'name'     :data.get('name')
-	             }
-	             
-		data_json = json.dumps(data_json)
-		for client_item in self.user_list:
-			yield from client_item.send(data_json)
+        data_json = {
+                    'user_list': list(self.user_list.values()),
+                    'name': data.get('name')
+                 }
 
-	@asyncio.coroutine
-	def onMessage(self,data=False,client=False):
-		"""Parsing messages from clients"""
-		data_json = {
-						'message'  :data.get('message',False),
-						'image'    :data.get('image'  ,False),
-						'name'     :data.get('name'   ,False),
-						'time'     :data.get('time'   ,False),
-		             }
-		data_json = json.dumps(data_json)
-		if client:
-			yield from client.send(data_json)
-		else:
-			for client_item in self.user_list:
-				yield from client_item.send(data_json)
+        data_json = json.dumps(data_json)
+        for client_item in self.user_list:
+            yield from client_item.send(data_json)
 
-def Enigma_match(name,token):
+    @asyncio.coroutine
+    def onMessage(self, data=False, client=False):
+        """Parsing messages from clients"""
 
-	if not (name and token):
-		return False
+        data_json = {
+                        'message': data.get('message', False),
+                        'image': data.get('image', False),
+                        'name': data.get('name', False),
+                        'time': data.get('time', False),
+                     }
 
-	hashes={
-				0:hashlib.md5,
-				1:hashlib.sha512,
-				2:hashlib.sha224,
-				3:hashlib.sha1
-	        }
+        data_json = json.dumps(data_json)
+        if client:
+            yield from client.send(data_json)
+        else:
+            for client_item in self.user_list:
+                yield from client_item.send(data_json)
 
-	kreator = hashes.get(int(token[0]),False)
 
-	if not kreator:
-		return False
-	else:
-		kreator = kreator()
-		kreator.update(name.encode())
+def Enigma_match(name, token):
+    if not (name and token):
+        return False
 
-	return token[1:] == kreator.hexdigest()
+    hashes = {
+                0: hashlib.md5,
+                1: hashlib.sha512,
+                2: hashlib.sha224,
+                3: hashlib.sha1
+            }
+
+    kreator = hashes.get(int(token[0]), False)
+
+    if not kreator:
+        return False
+    else:
+        kreator = kreator()
+        kreator.update(name.encode())
+
+    return token[1:] == kreator.hexdigest()
---
+++ server/main.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import asyncio
+import websockets
+import json
+import pymongo
+import time
+from lib import *
+
+CONN = pymongo.Connection('localhost', 27017)
+DB = CONN['async_db']
+COLLECTION_ROOMS = DB['rooms']
+COLLECTION_MESSAGES = DB['messages']
+
+# Восстанавливаем комнаты из бэкапа
+ROOM_DICT = {item['room_name']: Room_class(item.get('room_token', False)) for item in COLLECTION_ROOMS.find()}
+
+
+@asyncio.coroutine
+def server(client, url):
+    global ROOM_DICT
+
+    while client.open:
+        data = yield from client.recv()
+
+        # Ловим отключившихся
+        if data is None:
+            asyncio.Task(ROOM_DICT[room].onDisconnect(client, clean=True))
+            print('User {} disconnected'.format(ROOM_DICT[room].user_list[client]))
+
+        data = json.loads(data)
+        type_msg = data.get('type_msg', False)
+
+        crypt_test = Enigma_match(data.get('name'), data.get('token'))
+
+        if all([(type_msg == 'login'), data.get('room', False), crypt_test]):
+            room = data.get('room', False)
+            connect_trigger = [False, 'Access denied']
+            room_token = data.get('room_token', False)
+
+            if not ROOM_DICT.get(room, False):
+                ROOM_DICT[room] = Room_class(room_token)
+                connect_trigger[0] = True
+
+                # Бэкап списка комнат
+                doc = {
+                        'room_name': room,
+                        'room_token': room_token
+                       }
+                COLLECTION_ROOMS.save(doc)
+            else:
+                connect_trigger[0] = (ROOM_DICT[room].password == room_token)
+
+            if connect_trigger[0]:
+                connect_trigger = [
+                                   not data.get('name') in ROOM_DICT[room].user_list.values(),
+                                   "User already exists"
+                                   ]
+
+            Tasks = [
+                      lambda: ROOM_DICT[room].onDisconnect(client,reason=connect_trigger[1]),
+                      lambda: ROOM_DICT[room].onConnect(client, data)
+                    ]
+
+            asyncio.Task(Tasks[connect_trigger[0]]())
+
+            # Рассылаем логи
+            if connect_trigger[0]:
+                for item in COLLECTION_MESSAGES.find().sort('time'):
+                    asyncio.Task(ROOM_DICT[room].onMessage(data=item))
+
+                print('User {} conneced'.format(data.get('name', False)))
+
+        elif type_msg == 'message':
+            data['time'] = time.time()
+            data['name'] = ROOM_DICT[room].user_list[client]
+            asyncio.Task(ROOM_DICT[room].onMessage(data=data))
+
+            # Отправляем полученое сообщение в БД (храним логи)
+            doc = {
+                    'room_name': room,
+                    'message': data.get('message', False),
+                    'name': ROOM_DICT[room].user_list[client],
+                    'image': data.get('image', False),
+                    'time': time.time()
+                  }
+
+            COLLECTION_MESSAGES.save(doc)
+        else:
+            asyncio.Task(ROOM_DICT[room].onDisconnect(client))
+
+starter = websockets.serve(server, '0.0.0.0', 4042)
+
+asyncio.get_event_loop().run_until_complete(starter)
+asyncio.get_event_loop().run_forever()
